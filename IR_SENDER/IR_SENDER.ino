c2723fdc25aebe76f66393f31dff5f8b6f3f8bd9
Walrus <buttermunchinwalrus@gmail.com>
2024-11-04 14:23:55 -0500

    updated code

--- IR_SENDER/IR_SENDER.ino
+++ IR_SENDER/IR_SENDER.ino
@@ -6,40 +6,112 @@ decode_results results; //old depricated way of telling retuned we will use it s
 bool clearAlienCase = false; //we need a specefic hex code sent to make this true
 uint32_t testCase = 0xBA45FF00; //our current test case to ensure we are not getting garbage values
 uint32_t clearCase = 0xBA55FF00;
+uint32_t clearMessageCase = 0xBA75FF00;
 const int IR_Send_Pin = 13; //IR Sender Pin
 IRsend irsend(IR_Send_Pin); //initlize the ir library to the IR sender pin
+bool testClear = false;
+bool clearMessage = false;
+int globalIncrement = 0;
+char recieved[5];
 
 //millis()
 unsigned long time; // millis() is a unsinged long value
 
-//recieve and return hex value from ir reciever this funciton resumes recieving as of now
-uint32_t IrReceieve() {
-  if (IrReceiver.decode()) {
-  uint32_t hexValue = (IrReceiver.decodedIRData.decodedRawData, HEX); // store hexValue
-  irrecv.resume(); //resume reading for 
-  return hexValue;
-  }
-}
+//annot use in function
+// if (IrReceiver.decode()) {
+//       results.value = IrReceiver.decodedIRData.decodedRawData;
+//       Serial.println(results.value);
+//       irrecv.resume(); //resume reading for
+//       }
 
 //ability to send ir signals in a function
 void IrSend(uint32_t hexValue) {
+  irrecv.stop();
   irsend.sendNECRaw(hexValue, 32);
+  delay(50);
+  irrecv.enableIRIn();
 }
 
 //this function is to get a clear signal from the other tower
 void AlienConfirmSignal() {
   if (!clearAlienCase) {
-    if (IrReceieve() == testCase) {
-      for (int i = 0; i < 4; i++) {
+    if (IrReceiver.decode()) {
+    results.value = IrReceiver.decodedIRData.decodedRawData;
+    Serial.println(results.value);
+    irrecv.resume(); //resume reading for
+    }
+
+    if (results.value == testCase && !testClear) {
+      for (int i = 0; i < 5; i++) {
         IrSend(0xBA45FF00); //send 4 times to see if all 3 work
         Serial.println("Sending New Case");
       }
+      testClear = true;
+      results.value = 0;
     }
-    if (IrReceieve() == clearCase) {
+
+    if (results.value == clearCase) {
       clearAlienCase = true;
       Serial.println("ClearedAlienCase");
+      for (int i = 0; i < 6; i++) {
+        IrSend(clearCase);
+        Serial.println("sending cleared confirmed");
+      }
+    }
+    results.value = 0;
+  }
+}
+
+int StringSize(String str) {
+  int length = 0;
+  while (str[length] != '\0') {
+    length++;
+  }
+  return length;
+}
+
+void MessageStored() {
+  if (IrReceiver.decode()) {
+    results.value = IrReceiver.decodedIRData.decodedRawData;
+    Serial.println("PULSE WAVE DETECTED");
+    char x = char(results.value);
+    recieved[globalIncrement] = x;
+    globalIncrement++;
+    irrecv.resume(); //resume reading for
+
+    if (results.value == clearMessageCase) {
+      int length = StringSize(recieved);
+      for (int i = 0; i < globalIncrement; i++) {
+        Serial.print(recieved[i]);
+      }
+      globalIncrement = 0;
+      clearMessage = true;
+      return;
+      //return recieved;
     }
   }
+}
+
+void SendMessage() {
+  Serial.println("input message");
+  String input;
+  int length;
+    while (Serial.available() == 0) {
+  }
+    input = Serial.readString();
+    length = StringSize(input);
+
+    if (length > 0) {
+      for (int i = 0; i < length; i++) {
+        Serial.print("Sending: ");
+        Serial.println(input[i]);
+        IrSend(input[i]);
+      }
+      IrSend(clearMessageCase);
+      IrSend(clearMessageCase); //backup incase first fails
+      clearMessage = false;
+      return;
+    }
 }
 
 void setup() {
@@ -56,5 +128,24 @@ void setup() {
 }
 
 void loop() {
+  if(!clearAlienCase) {
   AlienConfirmSignal(); //only goes until it receives the clearCase
+  }
+  else {
+    if (!clearMessage) {
+      MessageStored();
+    }
+    else if (clearMessage){
+      SendMessage();
+    }
+  }
 }
+
+
+
+
+  // if (IrReceiver.decode()) {
+  //   results.value = IrReceiver.decodedIRData.decodedRawData;
+  //   Serial.println(char(results.value));
+  //   irrecv.resume(); //resume reading for
+  //   }
--- minecraft_CCINJECTOR/minecraft_CCINJECTOR.ino
+++ minecraft_CCINJECTOR/minecraft_CCINJECTOR.ino
@@ -1,14 +1,20 @@
-#include <IRremote.h>
+#include <IRremote.hpp>
 #include <Stepper.h>
 
 const int RECV_PIN = 10; //ir receiver pin
 IRrecv irrecv(RECV_PIN); //initlize the library with the pin
 decode_results results; //old depricated way of telling retuned we will use it since it works
-int clearComCount = 0; //make sure we actually recieve the correct values instead of garbage
+bool clearComCount = false; //make sure we actually recieve the correct values instead of garbage
 uint32_t testCase = 0xBA45FF00; //our current test case to ensure we are not getting garbage values
 uint32_t clearCase = 0xBA55FF00;
+uint32_t clearMessageCase = 0xBA75FF00;
 const int IR_Send_Pin = 13; //IR Sender Pin
 IRsend irsend(IR_Send_Pin); //initlize the ir library to the IR sender pin
+bool testclear = false;
+bool confirmed = false;
+bool clearMessage = false;
+int globalIncrement = 0;
+char recieved[5];
 
 //millis()
 unsigned long time; // millis() is a unsinged long value
@@ -17,51 +23,131 @@ unsigned long time; // millis() is a unsinged long value
 const int stepsPerRevolution = 200; //current turn amount for motor about 10-15%
 Stepper myStepper(stepsPerRevolution, 2, 3, 4, 5); //initlize the motor which is bipolar to the unipolar library :skull:
 
-//recieve and return hex value from ir reciever this funciton resumes recieving as of now
-uint32_t IrReceieve() {
-  if (IrReceiver.decode()) {
-  uint32_t hexValue = (IrReceiver.decodedIRData.decodedRawData, HEX);// think i need to push 0x infront
-  Serial.print("HEX: ");
-  Serial.println(hexValue, HEX);
-  irrecv.resume(); //resume reading for 
-  return (hexValue);
-  }
-}
+//Cannot use this in a function for some reason it breaks the program if you try
+// void IrReceieve() {
+//   if (IrReceiver.decode()) {
+//     results.value = IrReceiver.decodedIRData.decodedRawData;
+//     irrecv.resume(); //resume reading for
+//   }
+// }
 
 //ability to send ir signals in a function
 void IrSend(uint32_t hexValue) {
+  irrecv.stop();
   irsend.sendNECRaw(hexValue, 32);
+  delay(50);
+  irrecv.enableIRIn();
+  delay(50);
 }
 
 //this function is to get a clear signal from the other tower
 void ConfirmSignal() {
-  if (clearComCount > 3) {
+  if (clearComCount) {
+    Serial.println("WE ARE IN HERE");
+    delay(100);
+    results.value = 0;
+    if (IrReceiver.decode()) {
+      results.value = IrReceiver.decodedIRData.decodedRawData;
+      irrecv.resume(); //resume reading for
+      delay(50);
+      Serial.println(results.value);
+    }
+
+    if(results.value != clearCase) {
+      for (int i = 0; i < 2; i++) {
+        IrSend(clearCase);
+        Serial.println("Sending Clear Case");
+      }
+    }
+    else if (results.value == clearCase) {
+      Serial.println("WE ARE TRUE");
+      confirmed = true;
+    }
+    results.value = 0;
     return;
   }
 
   if (millis() - time > 1000) {
     Serial.println("Sending TestCase");
     IrSend(testCase);
+
     time = millis();
     while (millis() - time < 5000) {
+      delay(300);
+      Serial.println(results.value);
       Serial.println("WAITING");
-      if (IrReceieve() == testCase) {
-        clearComCount++;
-        Serial.println("We confimed IR signal adding com count");
-        time = millis(); //reset time so we can check again if it fails the millis wont get reset indefentily
+      if (IrReceiver.decode()) {
+      results.value = IrReceiver.decodedIRData.decodedRawData;
+      Serial.println(results.value);
+      irrecv.resume(); //resume reading for
       }
+
+      Serial.println(results.value);
+      if (results.value == testCase) {
+        clearComCount = true;
+        Serial.println("We confimed IR signal adding com count");
+        time = millis();
+        return;
+      }
+      results.value = 0;
     }
 
-    if (clearComCount >= 3) {
-      for (int i = 0; i < 4; i++) {
-        IrSend(clearCase);
-        Serial.println("Sending Clear Case");
-      }
-      time = millis();
-      return; // return so we dont accidently rotate on final iteration causing possible loss of signal
-    }
+    myStepper.step(stepsPerRevolution); //after this function actually does its checks then we can check the next area
     myStepper.step(stepsPerRevolution); //after this function actually does its checks then we can check the next area
     time = millis();
+  }
+}
+
+int StringSize(String str) {
+  int length = 0;
+  while (str[length] != '\0') {
+    length++;
+  }
+  return length;
+}
+
+void SendMessage() {
+  Serial.println("input message");
+  String input;
+  int length;
+    while (Serial.available() == 0) {
+  }
+    input = Serial.readString();
+    length = StringSize(input);
+
+    if (length > 0) {
+      for (int i = 0; i < length; i++) {
+        Serial.print("Sending: ");
+        Serial.println(input[i]);
+        IrSend(input[i]);
+      }
+      IrSend(clearMessageCase);
+      IrSend(clearMessageCase); //backup incase first fails
+      clearMessage = true;
+      return;
+    }
+}
+
+void MessageStored() {
+  if (IrReceiver.decode()) {
+    results.value = IrReceiver.decodedIRData.decodedRawData;
+    Serial.println("PULSE WAVE DETECTED");
+    char x = char(results.value);
+    recieved[globalIncrement] = x;
+    globalIncrement++;
+    irrecv.resume(); //resume reading for
+
+    if (results.value == clearMessageCase) {
+      int length = StringSize(recieved);
+      for (int i = 0; i < globalIncrement; i++) {
+        Serial.print(recieved[i]);
+        Serial.println();
+      }
+      globalIncrement = 0;
+      clearMessage = false;
+      return;
+      //return recieved;
+    }
   }
 }
 
@@ -78,27 +164,20 @@ void setup() {
   // irrecv.blink13(true); //see if ir receive gets anything it blinks on board
 
   time = millis(); //set time to whatever the current time is
+  results.value = 0;
 }
 
 void loop() {
-  // ConfirmSignal(); //will only go until it receives 3 valid signals
-  IrReceieve();
-  delay(200);
+  if (!confirmed) {
+    ConfirmSignal();
+  }
+  else {
+    if (!clearMessage)
+    SendMessage();
+    else if (clearMessage) {
+      MessageStored();
+    }
+  }
+  
+  // myStepper.step(stepsPerRevolution); //after this function actually does its checks then we can check the next area
 }
-
-
-
-
-
-// if (IrReceiver.decode()) {
-//     Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-//     if (IrReceiver.decodedIRData.decodedRawData == testcase) {
-//       Serial.println("We confirmed adding to count");
-//       clearComCount++;
-//     }
-//     irrecv.resume();
-//   }
-//   if (clearComCount < 3 && millis() - time >= 500) {
-//   Serial.println("Stepping Forward");
-//   myStepper.step(stepsPerRevolution);
-//   time = millis();
